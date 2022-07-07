def70fcf8a0046999ec9e03392165a826dca70d1
MiloCOLO <100871599+MiloCOLO@users.noreply.github.com>
2022-07-06 20:44:47 -0600

    added small code edits

--- assests/script.js
+++ assests/script.js
@@ -2,4 +2,14 @@ var date = moment().format('dddd, MMMM Do YYYY');
 var dateCalendar = moment().calendar();
 
 var prevSearch = [];
-// $('.search').on("click", functio)
+$('#search').on("click", function(event) {
+    event.preventDefault()
+    city = $(".searchBtn").val().trim()
+    console.log("FUCK")
+    if (city === "") {
+        return;
+    }
+    prevSearch.push(city);
+    localStorage.setItem("city", JSON.stringify(prevSearch));
+    console.log("HELLLLOOOO")
+});
